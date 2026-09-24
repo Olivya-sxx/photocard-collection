b72d9a295bc00b09dde2c9d8e192ca4c364fbbe0
Onna <onna@OnnadeMacBook-Air.local>
2026-09-24 22:39:10 +0800

    修复手机端更新缓存

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,11 @@
-const CACHE = 'photocard-collection-v4';
+const CACHE = 'photocard-collection-v5';
 const ASSETS = ['./', './index.html', './style.css', './mobile.css', './update.css', './crop.css', './app.js', './manifest.webmanifest', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
-self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request))));
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).then(response => {
+    if (event.request.url.startsWith(self.location.origin)) caches.open(CACHE).then(cache => cache.put(event.request, response.clone()));
+    return response;
+  }).catch(() => caches.match(event.request).then(hit => hit || caches.match('./'))));
+});
